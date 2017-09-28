677bf9353156892d64935ff3252c43d8b8b578fb
jonathan <jonathangtz.sosa@gmail.com>
2017-09-28 17:35:13 -0500

    initial tutorial

---
+++ main.kt
@@ -0,0 +1,19 @@
+
+fun main(args:Array<String>) {
+
+    // val -> para variables inmutables
+    val pizzaPrice:Int = 4
+    // pizzaPrice = 5 // No mepermite porquees inmutable
+
+    var hamburgerPrice:Int = 5
+    hamburgerPrice = 6
+
+    // Strings
+    //variable inferida, el compilador lo defineautomatico
+    val tocino = "Tocino"
+
+
+    println(tocino)
+    println("Hola mundo")
+
+}
